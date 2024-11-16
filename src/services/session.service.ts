5681efc9a131fc53517627f39b1e724d90deae3b
LinCie <aldiofernanda@protonmail.com>
2024-11-16 21:04:25 +0700

    feature: added basic session auth

---
+++ src/services/session.service.ts
@@ -0,0 +1,73 @@
+import { eq } from "drizzle-orm";
+import {
+  type User,
+  type Session,
+  sessionsTable,
+  usersTable,
+} from "@/db/schema";
+import { db } from "@/db";
+
+import {
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from "@oslojs/encoding";
+import { sha256 } from "@oslojs/crypto/sha2";
+
+export function generateSessionToken(): string {
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  return encodeBase32LowerCaseNoPadding(bytes);
+}
+
+export async function createSession(
+  token: string,
+  userId: number
+): Promise<Session> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session: Session = {
+    id: sessionId,
+    userId,
+    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+  };
+  await db.insert(sessionsTable).values(session);
+  return session;
+}
+
+export async function validateSessionToken(
+  token: string
+): Promise<SessionValidationResult> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const result = await db
+    .select({ user: usersTable, session: sessionsTable })
+    .from(sessionsTable)
+    .innerJoin(usersTable, eq(sessionsTable.userId, usersTable.id))
+    .where(eq(sessionsTable.id, sessionId));
+
+  if (result.length < 1) {
+    return { session: null, user: null };
+  }
+  const { user, session } = result[0];
+
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await db.delete(sessionsTable).where(eq(sessionsTable.id, sessionId));
+    return { session: null, user: null };
+  }
+
+  if (Date.now() >= session.expiresAt.getTime()) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await db
+      .update(sessionsTable)
+      .set({ expiresAt: session.expiresAt })
+      .where(eq(sessionsTable.id, sessionId));
+  }
+
+  return { session, user };
+}
+
+export async function invalidateSession(sessionId: string): Promise<void> {
+  await db.delete(sessionsTable).where(eq(sessionsTable.id, sessionId));
+}
+
+export type SessionValidationResult =
+  | { session: Session; user: User }
+  | { session: null; user: null };
