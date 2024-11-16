9f7480ebc40a55cc76da70ed6d5c8844793a607b
LinCie <aldiofernanda@protonmail.com>
2024-11-16 20:25:10 +0700

    feature: add sessions table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,12 @@
-import { mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
+import {
+  datetime,
+  int,
+  mysqlTable,
+  serial,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+import type { InferSelectModel } from "drizzle-orm";
 
 export const usersTable = mysqlTable("users", {
   id: serial().primaryKey(),
@@ -6,3 +14,16 @@ export const usersTable = mysqlTable("users", {
   name: varchar({ length: 255 }).notNull(),
   hash: varchar({ length: 255 }).notNull(),
 });
+
+export const sessionsTable = mysqlTable("sessions", {
+  id: varchar("id", {
+    length: 255,
+  }).primaryKey(),
+  userId: int("user_id")
+    .notNull()
+    .references(() => usersTable.id),
+  expiresAt: datetime("expires_at").notNull(),
+});
+
+export type User = InferSelectModel<typeof usersTable>;
+export type Session = InferSelectModel<typeof sessionsTable>;
