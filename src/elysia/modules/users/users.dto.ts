d08a96564d5d7d51e354d73ce1ef960bbf6419f7
LinCie <aldiofernanda@protonmail.com>
2024-11-17 15:10:02 +0700

    feat: add regex to dto objects
    added regex to create and update dto to strengten validation

--- src/elysia/modules/users/users.dto.ts
+++ src/elysia/modules/users/users.dto.ts
@@ -1,17 +1,47 @@
 import { t } from "elysia";
 
 const createUserDto = t.Object({
-  username: t.String({ minLength: 5, maxLength: 255 }),
-  name: t.String({ minLength: 5, maxLength: 255 }),
-  password: t.String({ minLength: 8, maxLength: 255 }),
+  username: t.String({
+    minLength: 5,
+    maxLength: 255,
+    pattern: "/^[a-zA-Z][a-zA-Z0-9_]*$/",
+  }),
+  name: t.String({
+    minLength: 5,
+    maxLength: 255,
+    pattern: "/^[A-Z][a-zA-Z'’-]*(?: [A-Z][a-zA-Z'’-]*)*$/",
+  }),
+  password: t.String({
+    minLength: 8,
+    maxLength: 255,
+    pattern: "/^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/",
+  }),
 });
 
 type CreateUserDto = typeof createUserDto.static;
 
 const updateUserDto = t.Object({
-  username: t.Optional(t.String({ minLength: 5, maxLength: 255 })),
-  name: t.Optional(t.String({ minLength: 5, maxLength: 255 })),
-  password: t.Optional(t.String({ minLength: 8, maxLength: 255 })),
+  username: t.Optional(
+    t.String({
+      minLength: 5,
+      maxLength: 255,
+      pattern: "/^[a-zA-Z][a-zA-Z0-9_]*$/",
+    })
+  ),
+  name: t.Optional(
+    t.String({
+      minLength: 5,
+      maxLength: 255,
+      pattern: "/^[A-Z][a-zA-Z'’-]*(?: [A-Z][a-zA-Z'’-]*)*$/",
+    })
+  ),
+  password: t.Optional(
+    t.String({
+      minLength: 8,
+      maxLength: 255,
+      pattern: "/^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/",
+    })
+  ),
 });
 
 type UpdateUserDto = typeof updateUserDto.static;
